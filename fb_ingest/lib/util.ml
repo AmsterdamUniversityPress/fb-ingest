f84ef7938a8df8c0a70c65941b2010de3dd67999
alleycat <git@alleycat.cc>
2024-02-14 18:09:25 +0000

    Small simplifications in util

--- fb_ingest/lib/util.ml
+++ fb_ingest/lib/util.ml
@@ -5,14 +5,14 @@ module String = struct
   let strip_spaces s =
     let f = function
       | ' ' -> Seq.empty
-      | x -> String.to_seq (String.make 1 x) in
+      | x -> Seq.return x in
     String.to_seq s
     |> Seq.flat_map f
     |> String.of_seq
-  let trim =
+  let trim x =
 	let sp = "[ \u{a0}]" in
 	let p = Fmt.str "^%s*(.*?)%s*$" sp sp in
 	let re = Re.Perl.compile_pat p in
 	let f groups = Re.Group.get groups 1 in
-	fun x -> Re.replace re ~f x
+	Re.replace re ~f x
 end
