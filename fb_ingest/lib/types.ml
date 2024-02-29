2b8c0797863fb849179f52b02c0a456b30c1ea15
alleycat <git@alleycat.cc>
2024-02-29 13:09:48 +0000

    Remove special value "Ja & cultuur"

--- fb_ingest/lib/types.ml
+++ fb_ingest/lib/types.ml
@@ -421,18 +421,13 @@ module Column = struct
   let mk (Column t) = t.mk
 end
 
-(* let validate_bool = {| (Ja|ja|Nee|nee) |} *)
-(* --- @todo until they fix the data *)
-let validate_bool = {| (Ja|ja|Nee|nee|Ja \s+ \& \s+ Cultuur) |}
+let validate_bool = {| (Ja|ja|Nee|nee) |}
 let validate_int = {| -? \d+ |}
 let validate_text = {| .+ |}
 
 let _validate_url_strict = {| (https?://)? ((\w|-)+\.)* (\w|-)+ (/ [\w \d % \( \) \. _ -]*)* (#[\w\d%-]*)? (\? [\w \d \( \) % & = ; \. -]*)? |}
 let _validate_url_full =
   {| (?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?: |} ^
-  (* (?!(?:10|127)(?:\.\d{1,3}){3}) *)
-  (* (?!(?:169\.254|192\.168)(?:\.\d{1,3}){2}) *)
-  (* (?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2}) *)
   {| (?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?: |} ^
   "[a-z0-9\u{00a1}-\u{ffff}][a-z0-9\u{00a1}-\u{ffff}_-]{0,62})?" ^
   "[a-z0-9\u{00a1}-\u{ffff}]\\.)+(?:[a-z\u{00a1}-\u{ffff}]" ^
@@ -445,8 +440,6 @@ let bool_of_string_nl = function
   | "ja" -> true
   | "Nee" -> false
   | "nee" -> false
-  (* --- @todo until they fix the data *)
-  | "Ja & Cultuur" -> true
   | x -> failwith (Fmt.str "Can't convert %s to bool" x)
 
 let col_id = Column.Column {
